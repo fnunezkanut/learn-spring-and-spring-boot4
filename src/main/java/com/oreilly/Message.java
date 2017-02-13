555e29cb7fe9d40aab1725095f7c425525665310
fnunezkanut <fnunezkanut@users.noreply.github.com>
2017-02-13 18:46:57 +0000

    55

--- src/main/java/com/oreilly/Message.java
+++ src/main/java/com/oreilly/Message.java
@@ -1,20 +1,16 @@
 package com.oreilly;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-//@Component
 public class Message {
 
-	//@Value("${my.messageValue}")
-	private int messageValue;
+	private String messageValue;
 
 	//getters and setters
-	public int getMessageValue() {
+	public String getMessageValue() {
 		return messageValue;
 	}
 
-	public void setMessageValue(int messageValue) {
+	public void setMessageValue(String messageValue) {
 		this.messageValue = messageValue;
 	}
 }
--- src/main/java/com/oreilly/config/ApplicationConfiguration.java
+++ src/main/java/com/oreilly/config/ApplicationConfiguration.java
@@ -4,29 +4,16 @@ import com.oreilly.Message;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ApplicationConfiguration {
 
 	@Bean
-	@Profile("prod")
-	public Message myMessage(  ){
+	public Message myMessage( @Value("${my.message}") String str ){
 
 		Message message = new Message();
 
-		message.setMessageValue( 1 );
-
-		return message;
-	}
-
-	@Bean
-	@Profile("dev")
-	public Message myMessageDev( ){
-
-		Message message = new Message();
-
-		message.setMessageValue( 0 );
+		message.setMessageValue( str );
 
 		return message;
 	}
